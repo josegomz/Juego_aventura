c2f8710a99884c3595e0de691d2fda60318f6ce9
josegomz <josegomz.sa17@gmail.com>
2020-04-01 14:26:29 -0600

    configuraciones

--- src/Game/interfaz/Componentes.java
+++ src/Game/interfaz/Componentes.java
@@ -25,19 +25,37 @@ public class Componentes {
         initComponents();
     }
             
-    public void initComponents(){
+    private void initComponents(){
         //personalizar el fondo
         fondo = new JLabel();
         fondo.setSize(1000,700);
-        fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/game/img/bg/BG1.png"))); // NOI18N
-        
+        fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/bg/BG1.png"))); // NOI18N
         //boton cerrar
         btn_cerrar = new JLabel();
-        btn_cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/game/img/bg/BG1.png"))); // NOI18N
-        
+        btn_cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/salir_01.png"))); // NOI18N
+        btn_cerrar.setSize(70, 70);
+        btn_cerrar.setLocation(920, 10);
+        btn_cerrar.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btn_cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/salir_03.png"))); 
+            }
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt){
+                btn_cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/salir_02.png"))); 
+            }
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt){
+                btn_cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/salir_01.png"))); 
+            }
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent evt){
+                btn_cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/salir_02.png"))); 
+            }
+        });
     }
      public void playAudio(String nombre){
-         audio = Applet.newAudioClip(getClass().getClassLoader().getResource("/game/music/"+nombre));
+         audio = Applet.newAudioClip(getClass().getClassLoader().getResource("/Game/music/"+nombre));
          audio.loop();
      }
      
--- src/Game/interfaz/Menu.java
+++ src/Game/interfaz/Menu.java
@@ -5,18 +5,66 @@
  */
 package Game.interfaz;
 
+import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author macbookair
  */
-public class Menu extends Thread{
+public class Menu extends JFrame{
     Componentes componentes;
-    
+    JPanel panel_menu;
+
     public Menu(){
-        
+        componentes = new Componentes();
+        initComponents();
+        componentsFrame();
+        componentes.playAudio("Osondoar.wav");
     }
     
-    public static void main(String[] args) {
+    private void initComponents(){
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        setResizable(false);
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1000, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 700, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(null);
+        panel_menu = new JPanel();
+
+        GroupLayout grupo = new GroupLayout(panel_menu);
+        panel_menu.setLayout(grupo);
+        panel_menu.setSize(1000, 700);
+        panel_menu.setBackground(Color.GREEN);   
         
+        panel_menu.add(componentes.btn_cerrar);
+        panel_menu.add(componentes.fondo);
     }
+    
+    private void componentsFrame() {
+        getContentPane().add(panel_menu);
+    }
+   
+   
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Menu().setVisible(true);
+            }
+        });
+    }
+
+    
 }
