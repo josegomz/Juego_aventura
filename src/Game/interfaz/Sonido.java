5eb357026749f7de7b34244173bbff27bace38fe
josegomz <josegomz.sa17@gmail.com>
2020-04-02 19:45:09 -0600

    Sonido click

--- src/Game/interfaz/Sonido.java
+++ src/Game/interfaz/Sonido.java
@@ -5,10 +5,26 @@
  */
 package Game.interfaz;
 
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  *
  * @author macbookair
  */
 public class Sonido {
-    
+    public Clip clip;
+    public String ruta = "/Game/music/";
+    public void sonido(String archivo){
+        try{
+            clip=AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(getClass().getResourceAsStream(ruta+archivo)));
+            clip.start();
+        }catch(IOException | LineUnavailableException | UnsupportedAudioFileException e){
+            System.out.println("Error de sonido");
+        }
+    }
 }
