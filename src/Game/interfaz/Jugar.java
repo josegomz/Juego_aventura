e1ab04877d21d8baa199cc77a268fdf10ab1ee2d
josegomz <josegomz.sa17@gmail.com>
2020-04-30 07:28:15 -0500

    mas recursos y edicion del login con funciones en el juego

--- src/Game/interfaz/Jugar.java
+++ src/Game/interfaz/Jugar.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//nombre del paquete
 package Game.interfaz;
 
 import java.applet.AudioClip;
@@ -16,23 +12,29 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author macbookair
+ * @author Josegomz
  */
 public class Jugar extends JFrame {
-
+    final int NUM_OBSTACULOS=5;
+    //variables de objeto
     Movimiento hilo;
     Sonido sound;
     Jugador jugador;
     Componentes componentes;
+    Obstaculos obstaculos[];
     AudioClip audio;
+    
     JPanel panel_jugar;
-
+    JLabel[] fondo;
+    JLabel[] tile_01;
     JLabel btn_home;
     JLabel btn_pausa;
 
     public Jugar(String genero) {
-        jugador = new Jugador(genero, 20, 320);
+        this.paso = new int[2];
+        jugador = new Jugador(genero, 20, 390);
         sound = new Sonido();
+
         componentes = new Componentes();
         initFrame();
         initComponentes();
@@ -45,8 +47,13 @@ public class Jugar extends JFrame {
 
     private void initComponentes() {
         panel_jugar = componentes.getPanel();
-
         btn_home = new JLabel();
+        
+        obstaculos = new Obstaculos[NUM_OBSTACULOS];
+        for (int i = 0; i < NUM_OBSTACULOS; i++) {
+            obstaculos[i]= new Obstaculos(i*450+300, 500);
+            obstaculos[i].agregar();
+        }
         btn_home.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/home_01.png")));
         btn_home.setSize(70, 70);
         btn_home.setLocation(10, 10);
@@ -81,6 +88,7 @@ public class Jugar extends JFrame {
             }
 
         });
+        
         btn_pausa = new JLabel();
         btn_pausa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/pausa_01.png")));
         btn_pausa.setSize(70, 70);
@@ -112,12 +120,29 @@ public class Jugar extends JFrame {
             }
 
         });
-
+        tile_01 = new JLabel[20];
+        for (int i = 0; i < 20; i++) {
+            tile_01[i] = new JLabel();
+            tile_01[i].setSize(60, 60);
+            tile_01[i].setLocation((60 * i), 640);
+            tile_01[i].setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/tiles/2.png")));
+            panel_jugar.add(tile_01[i]);
+        }
+        
+        fondo = new JLabel[2];
+        for (int i = 0; i < 2; i++) {
+            fondo[i] = new JLabel();
+            fondo[i].setSize(1000, 700);
+            fondo[i].setLocation(i * 1000, 0);
+            fondo[i].setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/bg/BG1.png"))); // NOI18N
+        }
+        
         panel_jugar.add(jugador.player);
         panel_jugar.add(btn_home);
         panel_jugar.add(btn_pausa);
         panel_jugar.add(componentes.btn_cerrar);
-        panel_jugar.add(componentes.fondo);
+        panel_jugar.add(fondo[0]);
+        panel_jugar.add(fondo[1]);
     }
 
     private void initFrame() {
@@ -138,9 +163,15 @@ public class Jugar extends JFrame {
                 switch (kevt) {
                     case KeyEvent.VK_D:
                         estadoderecha = true;
+                        caminando = true;
+                        paso[0]= -20;
+                        paso[1] = -5;
                         break;
                     case KeyEvent.VK_A:
                         estadoizquierda = true;
+                        caminando = true;
+                        paso[0]= 20;
+                        paso[1] = 5;
                         break;
                     case KeyEvent.VK_SPACE:
                         salto = true;
@@ -155,6 +186,7 @@ public class Jugar extends JFrame {
                 int kevt = evt.getExtendedKeyCode();
                 if (kevt == KeyEvent.VK_D) {
                     estadoderecha = false;
+                    caminando = false;
                 } else if (kevt == KeyEvent.VK_A) {
                     estadoizquierda = false;
                 }
@@ -166,44 +198,73 @@ public class Jugar extends JFrame {
     private void componetesframe() {
         getContentPane().add(panel_jugar);
     }
+    
+    //variables de estado del jugador
     private boolean estadoderecha;
     private boolean estadoizquierda;
     private boolean salto;
+    private boolean caminando;
+    private int[] paso ={-20,-3};
 
+    //hilo que se encarga del movimiento
     private class Movimiento extends Thread {
-
         @Override
         public void run() {
             while (true) {
                 System.out.println("");
+                if (caminando) {
+                    for (int j = 0; j < NUM_OBSTACULOS; j++) {
+                            obstaculos[j].mover(paso[0]);
+                    }
+                    for (int i = 0; i < 20; i++) {
+                        tile_01[i].setLocation(tile_01[i].getX() + paso[0], tile_01[i].getY());
+                        if (tile_01[i].getX() == -60 && paso[0]== -20) {
+                            tile_01[i].setLocation(19 * 60, tile_01[i].getY());
+                        }
+                        else if (tile_01[i].getX() == 19*60 && paso[0] == 20) {
+                            tile_01[i].setLocation(-60, tile_01[i].getY());
+                        }
+                    }
+                    for (int i = 0; i < 2; i++) {
+                        fondo[i].setLocation(fondo[i].getX() + paso[1], 0);
+                        if (fondo[i].getX() == -1000 &&paso[i]== -3) {
+                            fondo[i].setLocation(1000, 0);
+                        }
+                        else if (fondo[i].getX() == 1000 && paso[i]== 3) {
+                            fondo[i].setLocation(-1000, 0);
+                        }
+                    }
+                }
                 if (estadoderecha) {
                     sound.sonido("sfx_step_rock_r.wav");
                     for (int i = 1; i <= 8; i++) {
                         try {
-                            Thread.sleep(50);
+                            Thread.sleep(30);
                             jugador.paint_walk(i);
                             if (i == 4) {
                                 sound.sonido("sfx_step_rock_r.wav");
                             }
-                            jugador.setLocation(jugador.getX() + 10, jugador.getY());
-                            Thread.sleep(50);
+                            if (jugador.getX()<830) {
+                                jugador.setLocation(jugador.getX() + 10, jugador.getY());
+                            }
+                            Thread.sleep(30);
                         } catch (InterruptedException ex) {
                             Logger.getLogger(Jugar.class.getName()).log(Level.SEVERE, null, ex);
                         }
                     }
-
+                    
                 }
                 if (estadoizquierda) {
                     sound.sonido("sfx_step_rock_l.wav");
                     for (int i = 1; i <= 8; i++) {
                         try {
-                            Thread.sleep(50);
+                            Thread.sleep(35);
                             jugador.paint_walk(i + 8);
                             if (i == 4) {
                                 sound.sonido("sfx_step_rock_r.wav");
                             }
                             jugador.setLocation(jugador.getX() - 10, jugador.getY());
-                            Thread.sleep(50);
+                            Thread.sleep(35);
                         } catch (InterruptedException ex) {
                             Logger.getLogger(Jugar.class.getName()).log(Level.SEVERE, null, ex);
                         }
@@ -221,32 +282,62 @@ public class Jugar extends JFrame {
                     }
                 }
                 if (salto) {
+                    int salt = 15;
                     sound.sonido("sfx_step_rock_l.wav");
-                    for (int i = 1; i <= 8; i++) {
+                    for (int i = 1; i <= 15; i++) {
                         try {
-                            Thread.sleep(50);
+                            Thread.sleep(20);
                             jugador.paint_jump(i);
-                            jugador.setLocation(jugador.getX(), jugador.getY() - 15);
-                            Thread.sleep(50);
+                            jugador.setLocation(jugador.getX(), jugador.getY() - salt);
+                            Thread.sleep(20);
                         } catch (InterruptedException ex) {
                             Logger.getLogger(Jugar.class.getName()).log(Level.SEVERE, null, ex);
                         }
-                    }
-                    for (int i = 9; i <= 15; i++) {
-                        try {
-                            Thread.sleep(30);
-                            jugador.paint_jump(i);
-                            jugador.setLocation(jugador.getX(), jugador.getY() + 15);
-                            Thread.sleep(30);
-                        } catch (InterruptedException ex) {
-                            Logger.getLogger(Jugar.class.getName()).log(Level.SEVERE, null, ex);
+                        if (i == 8) {
+                            salt = -salt;
                         }
                     }
+
                     jugador.setLocation(jugador.getX(), jugador.getY() + 15);
                     sound.sonido("sfx_step_rock_l.wav");
                     salto = false;
                 }
             }
+        }
+    }
+    
+    private class Obstaculos{
+        int posX;
+        int posY;
+        JLabel[] tile_02;
+        
+
+        public Obstaculos(int posX, int posY) {
+            this.posX = posX;
+            this.posY = posY;
+            initComponents();
+        }
+
+        private void initComponents() {
+            tile_02 = new JLabel[3];
+            for (int i = 0; i < 3; i++) {
+                tile_02[i] = new JLabel();
+                tile_02[i].setSize(83, 60);
+                tile_02[i].setLocation(posX+(i*83), posY);
+                tile_02[i].setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/tiles/"+(13+i)+".png")));
+                
+            }
+        }
+        
+        private void agregar(){
+            panel_jugar.add(tile_02[0]);
+            panel_jugar.add(tile_02[1]);
+            panel_jugar.add(tile_02[2]);
+        }
+        
+        private void mover(int paso){
+            for (int i = 0; i < 3; i++)
+                tile_02[i].setLocation(tile_02[i].getX() + paso, tile_02[i].getY());
         }
     }
 
---
+++ src/Game/interfaz/Login.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game.interfaz;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.GroupLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author josegomz
+ */
+public class Login extends JFrame{
+    JPanel panel_login;
+    Componentes componentes;
+    Sonido sound;
+    
+    private JLabel lbl_username;
+    private JLabel lbl_password;
+    private JLabel btn_ok;
+    
+    private JTextField txt_username;
+    private JPasswordField txt_password;
+    
+    private Component confirmation;
+    String usuario = "root";
+    String password = "root";
+    
+    //constructor
+    public Login(){
+        componentes = new Componentes();
+        sound = new Sonido();
+        initFrame();
+        initComponents();
+        componentsFrame();
+    }
+
+    private void initFrame() {
+        //personalizar el FRAME
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 400, Short.MAX_VALUE));
+        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 350, Short.MAX_VALUE));
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    private void initComponents() {
+        panel_login = new JPanel();
+        GroupLayout grupo = new GroupLayout(panel_login);
+        panel_login.setLayout(grupo);
+        panel_login.setSize(400, 350);
+        panel_login.setBackground(Color.red);
+        
+        componentes.fondo.setLocation(-200, -200);
+        btn_ok = new JLabel();
+        btn_ok.setSize(200, 67);
+        btn_ok.setLocation(100, 270);
+        btn_ok.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/entrar_01.png"))); // NOI18N
+        btn_ok.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btn_ok.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/entrar_03.png")));
+                sound.sonido("click.wav");
+            }
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_ok.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/entrar_02.png")));
+                sound.sonido("click2.wav");
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_ok.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/entrar_01.png")));
+            }
+
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                btn_ok.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Game/img/gui/botones/entrar_01.png")));
+                Menu menu = new Menu();
+                menu.show();
+                dispose();
+                        
+            }
+        });
+        lbl_username = new JLabel();
+        lbl_password = new JLabel();
+        lbl_username.setSize(300, 50);
+        lbl_password.setSize(300, 50);
+        lbl_username.setLocation(25, 70);
+        lbl_password.setLocation(25, 160); 
+        try {
+            lbl_username.setFont(componentes.getFont(30));
+            lbl_password.setFont(componentes.getFont(30));
+
+        } catch (FontFormatException | IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }       
+        lbl_username.setText("User");
+        lbl_password.setText("Password");
+        
+        txt_username = new JTextField();
+        txt_username.setSize(350, 50);
+        txt_username.setLocation(25, 110);
+        txt_username.setBackground(new java.awt.Color(213, 173, 81));
+        txt_username.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        try {
+            txt_username.setFont(componentes.getFont(30));
+        } catch (FontFormatException | IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        txt_username.setBorder(null);
+        
+        txt_password = new JPasswordField();
+        txt_password.setSize(350, 50);
+        txt_password.setLocation(25, 200);
+        txt_password.setBackground(new java.awt.Color(213, 173, 81));
+        txt_password.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txt_password.setBorder(null);
+        
+        componentes.titulo.setForeground(Color.BLACK);
+        componentes.titulo.setLocation(0, 10);
+        componentes.titulo.setSize(400, 50);
+        componentes.titulo.setText("Login");
+        
+        panel_login.add(lbl_username);
+        panel_login.add(lbl_password);
+        panel_login.add(txt_username);
+        panel_login.add(txt_password);
+        panel_login.add(btn_ok);
+        panel_login.add(componentes.titulo);
+        panel_login.add(componentes.fondo);
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
+    private void componentsFrame() {
+        getContentPane().add(panel_login);
+    }
+    
+}
